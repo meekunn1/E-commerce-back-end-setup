1b70995dc39b4af33cca9723ca70e448faded04f
Keiji O <meekunn@gmail.com>
2023-07-05 22:42:50 -0700

    seed issue fixed. category had typo

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,8 +40,14 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {where: {id: req.params.id,}});
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
