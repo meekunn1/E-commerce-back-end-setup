93ba65dc3d14d139b2cfa680dc8455003b30b9eb
Keiji O <meekunn@gmail.com>
2023-07-06 00:21:18 -0700

    all routes functional. Insomnia tested

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // find all products
   try {
     const productData = await Product.findAll({
-      include: [{model: Category, Tag}]
+      include: [{model: Category}, {model: Tag}]
         // be sure to include its associated Category and Tag data
     });
     res.status(200).json(productData);
@@ -18,13 +18,22 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag}]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
 router.post('/', (req, res) => {
+
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -100,8 +109,14 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({where: {id: req.params.id}});
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
